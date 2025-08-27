82a325dba323ea3b15da25c80aea45cc279c253c
Marian Sheehy <marianjc80@gmail.com>
2025-08-27 22:59:39 +0100

    Movie Details Page rendering.

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,31 +1,47 @@
 export interface BaseMovieProps {
-    title: string;
-    budget: number;
-    homepage: string | undefined;
-    id: number;
-    imdb_id: string;
-    original_language: string;
-    overview: string;
-    release_date: string;
-    vote_average: number;
-    popularity: number;
-    poster_path?: string;
-    tagline: string;
-    runtime: number;
-    revenue: number;
-    vote_count: number;
-    favourite?: boolean;
-  }
+  title: string;
+  budget: number;
+  homepage: string | undefined;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  overview: string;
+  release_date: string;
+  vote_average: number;
+  popularity: number;
+  poster_path?: string;
+  tagline: string;
+  runtime: number;
+  revenue: number;
+  vote_count: number;
+  favourite?: boolean;
+}
 
 export interface BaseMovieListProps { 
-    movies: BaseMovieProps[];
-  }
+  movies: BaseMovieProps[];
+}
 
 export interface MovieDetailsProps extends BaseMovieProps {
-    genres: {
-      id: number;
-      name: string;
-    }[];
-  }
+  genres: {
+    id: number;
+    name: string;
+  }[];
+}
+
+export interface MovieImage {
+  file_path: string;
+  aspect_ratio?: number; //some props are optional...
+  height?: number;
+  iso_639_1?: string;
+  vote_average?: number;
+  vote_count?: number;
+  width?: number;
+} 
+
+export interface MoviePageProps {
+  movie: MovieDetailsProps;
+  images: MovieImage[];
+}
+
 
 
